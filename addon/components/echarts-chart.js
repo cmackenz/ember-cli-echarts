0be90448a12869e8eb6e51e2d515d64f9ea54ad3
Craig MacKenzie <craig@funnelcloud.io>
2018-02-12 18:56:22 -0500

    initial commit (wip)

---
+++ addon/components/echarts-chart.js
@@ -0,0 +1,76 @@
+import Ember from 'ember';
+import echarts from 'ember-echarts';
+
+const {
+  Component,
+  get,
+  set,
+  computed,
+  observer,
+  run,
+  on,
+  $
+} = Ember;
+
+export default Component.extend({
+  classNames: ['echarts-chart'],
+
+  options: {},
+  notMerge: false,
+  lazyUpdate: false,
+  theme: null,
+
+  _chart: null,
+
+  beforeSetup(){},
+  afterSetup(){},
+
+  init(){
+    this._super(...arguments);
+    $(window).on('resize', this.handleResize.bind(this));
+  },
+
+  eventContext: computed(function(){
+    return get(this, 'targetObject') || this;
+  }),
+
+  reDraw: on('didInsertElement', observer('options', 'options.{*}', function(){
+    run.scheduleOnce('render', this, this.drawChart);
+  })),
+
+  drawChart(){
+    if (get(this, '_chart') && get(this, '_chart').isDisposed()) {
+      return;
+    }
+
+    let chart;
+    let selector = '#' + get(this, 'elementId');
+    let $el = $(selector);
+    let context = get(this, 'eventContext');
+
+    chart = echarts.init($el[0]);
+
+    set(this, '_chart', chart);
+
+    run(() => get(this, 'beforeSetup').call(context, chart));
+
+    chart.setOption(get(this, 'options'), get(this, 'notMerge'), get(this, 'lazyUpdate'));
+
+    run(() => get(this, 'afterSetup').call(context, chart));
+
+    return chart;
+  },
+
+  handleResize(){
+    const chart = get(this, '_chart');
+    if (chart) chart.resize();
+  },
+
+  willDestroyElement(){
+    this._super(...arguments);
+    const chart = get(this, '_chart');
+    if (chart) chart.dispose();
+    $(window).off("resize");
+  },
+
+});
